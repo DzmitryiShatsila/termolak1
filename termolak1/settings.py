ce1e33a59dd47bae347d4aac6624353027459833
Dzmitryi Shatsila <shatilo124@gmail.com>
2020-05-15 17:13:14 +0300

    test gmail 2

--- termolak1/settings.py
+++ termolak1/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-# SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-# DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
+# DEBUG = False
+DEBUG = os.environ.get('DJANGO_DEBUG', True)
 
 ALLOWED_HOSTS = ['termolak1.herokuapp.com', '127.0.0.1']
 
