e5bc7878f0439f91a48aa4ba0a04a951d63b4858
Dzmitryi Shatsila <shatilo124@gmail.com>
2020-05-18 23:28:06 +0300

    Change settings

--- termolak1/settings.py
+++ termolak1/settings.py
@@ -130,8 +130,8 @@ STATIC_URL = '/static/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'django.tests.dzmitryi@gmail.com'
-EMAIL_HOST_PASSWORD = 'Arve2018'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
